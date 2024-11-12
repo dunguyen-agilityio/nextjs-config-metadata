4dd638793b4d08132eccf5d72b481a78858475c2
dunguyen-agilityio <140130209+dunguyen-agilityio@users.noreply.github.com>
2024-11-12 07:11:38 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About",
+  description: "This is About page!",
+};
+
+const AboutPage = () => {
+  return <div>AboutPage</div>;
+};
+
+export default AboutPage;
